ecba36c3783251d078b620be191809904476c5c1
dylanlawless79 <dylan.lawless@gmail.com>
2018-05-24 13:36:38 +0100

    remote config for images\text in any fragment braintree paypal (you need to add server code into AppGlobal) all working

--- app/src/main/java/uk/co/apptouch/apptouch30/AppGlobals.java
+++ app/src/main/java/uk/co/apptouch/apptouch30/AppGlobals.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 public class AppGlobals extends Application {
 
-    public static final String SERVER_IP = "http://138.68.170.85:9000";
-    public static final String BASE_URL = String.format("%s/api/", SERVER_IP);
+
 
 }
